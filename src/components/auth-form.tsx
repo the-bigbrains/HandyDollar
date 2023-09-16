1d28104479411ffa1e276202ff46ff698b71f7f1
dany <71732131+DanyPalma@users.noreply.github.com>
2023-09-16 00:30:33 -0400

    push with user auth

---
+++ src/components/auth-form.tsx
@@ -0,0 +1,20 @@
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from './database.types'
+
+export default function AuthForm() {
+  const supabase = createClientComponentClient<Database>()
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      onlyThirdPartyProviders={true}
+      appearance={{ theme: ThemeSupa }}
+      theme="dark"
+      showLinks={false}
+      providers={['github']}
+      redirectTo="http://localhost:3000/auth/callback"
+    />
+  )
+}
