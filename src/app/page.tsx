c671b644612e95f159e76559e10974fdeab8ae01
dany <71732131+DanyPalma@users.noreply.github.com>
2023-09-16 01:07:03 -0400

    user login / signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import AuthForm from './auth-form'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-    </main>
+    <div className="flex flex-col justify-center items-center">
+      <div className="col-6">
+        <h1 className="header">Supabase Auth + Storage</h1>
+        <p className="">
+          Experience our Auth and Storage through a simple profile management example. Create a user
+          profile and upload an avatar image. Fast, simple, secure.
+        </p>
+      </div>
+      <div className="col-6 auth-widget">
+        <AuthForm />
+      </div>
+    </div>
   )
 }
