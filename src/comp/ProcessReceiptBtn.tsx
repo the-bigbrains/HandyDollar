b8950e9c544a7382ee9c2a15963ba1e3dbb78904
Sim Ho <simhozebs@gmail.com>
2023-09-16 20:17:01 -0400

    test returns to value

--- src/comp/ProcessReceiptBtn.tsx
+++ src/comp/ProcessReceiptBtn.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-export default async function Page() {
+export default function Page() {
   const imgURL =
     "https://qph.cf2.quoracdn.net/main-qimg-5a0fda721e2f66b6042bde9a10e93d83-lq";
 
   const test = async () => {
-    await fetch(`http://localhost:3000/api/test`, {
+    return await fetch(`http://localhost:3000/api/test`, {
       method: "POST",
       body: JSON.stringify(imgURL),
       cache: "no-store",
