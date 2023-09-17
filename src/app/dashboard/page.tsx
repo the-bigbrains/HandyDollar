99e525b9d9dbfe7b5e1c451e935dc9c65a83010c
dany <71732131+DanyPalma@users.noreply.github.com>
2023-09-17 07:52:26 -0400

    fix account dropdown

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import Graph1 from "./Graph1";
 import Moneys from "./Moneys";
 import AccountInfo from "./accountInfo";
 import UploadChoice from "./UploadChoice";
+import { cookies } from "next/headers";
+import { Database } from "@/types/supabase";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const sandboxToken = "access-sandbox-4b5dcec0-fbfd-4ba4-8db8-1fd4eee03111";
 
@@ -13,6 +16,13 @@ const sandboxToken = "access-sandbox-4b5dcec0-fbfd-4ba4-8db8-1fd4eee03111";
 
 export default async function Dashboard() {
   //sandbox access key generated from setAccessToken()
+  
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
   const txArray = await sync(sandboxToken);
 
   const moneySpent = txArray.reduce((total, tx) => {
@@ -56,7 +66,7 @@ export default async function Dashboard() {
       <div className="text-purple-300 py-4 px-8 flex border-b border-gray-600 items-center">
         <div className="mr-auto text-3xl">Dashboard</div>
         <div className="px-8 hover:cursor-pointer hover:underline">
-          <AccountInfo />
+          <AccountInfo session={session}/>
         </div>
         <div className="px-8 hover:cursor-pointer hover:underline">
           <UploadChoice />
