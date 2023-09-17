534d441cb8f285c5893fbd306fca8a6df1ad4e9c
Sim Ho <simhozebs@gmail.com>
2023-09-17 03:52:16 -0400

    bunch of bug fix

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { Database } from "@/types/supabase";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-export const supabaseServer = createServerComponentClient<Database>({
-  cookies,
-});
