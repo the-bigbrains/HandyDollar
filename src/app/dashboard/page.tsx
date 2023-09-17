e98b10e70673a9d7c55f8faa8bddc0035c3512d5
dany <71732131+DanyPalma@users.noreply.github.com>
2023-09-17 01:27:17 -0400

    push recharts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import sync from "@/lib/sync";
 import TransactionCardList from "./TxCardList";
-import Graph from "./Graph";
+import Graph1 from "./Graph1";
 import Moneys from "./Moneys";
 import Link from "next/link";
-
 
 const sandboxToken = "access-sandbox-4b5dcec0-fbfd-4ba4-8db8-1fd4eee03111";
 
@@ -12,6 +11,7 @@ const sandboxToken = "access-sandbox-4b5dcec0-fbfd-4ba4-8db8-1fd4eee03111";
 // make a function to loop over txArray and add up all the positive values and return it
 
 export default async function Dashboard() {
+
   //sandbox access key generated from setAccessToken()
   const txArray = await sync(sandboxToken);
 
@@ -81,7 +81,7 @@ export default async function Dashboard() {
         />
       </div>
       <div className="w-full flex flex-row h-full px-20">
-        <Graph />
+        <Graph1 data={txArray}/>
         <div className="w-full rounded-xl flex flex-col items-center justify-start outline-gray-500 outline mb-5">
           <div className="flex flex-col w-full">
             <div className="text-4xl mx-4 my-2">Transactions</div>
