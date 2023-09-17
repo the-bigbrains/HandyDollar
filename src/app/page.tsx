e742087b1c95fe7d96074be23d888dbc4e21d600
dany <71732131+DanyPalma@users.noreply.github.com>
2023-09-17 10:27:07 -0400

    fix login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "./auth-form";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-full w-full text-center">
+    <div className="flex flex-col justify-center items-center h-screen w-full text-center">
       <div className="col-6">
         <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-gray-500 pb-3">
           Welcome to SandyDollar!
