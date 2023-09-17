86d00b1208696cf93722b2f3927b65b8cb465795
Sim Ho <simhozebs@gmail.com>
2023-09-17 10:54:14 -0400

    fix

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '../types/supabase'
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../types/supabase";
 
 export default function AuthForm() {
-  const supabase = createClientComponentClient<Database>()
+  const supabase = createClientComponentClient<Database>();
 
   return (
     <Auth
@@ -14,8 +14,8 @@ export default function AuthForm() {
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       showLinks={false}
-      providers={['github']}
-      redirectTo="http://localhost:3000/auth/callback"
+      providers={["github"]}
+      redirectTo="https://handy-dollars.vercel.app/auth/callback"
     />
-  )
+  );
 }
--- src/lib/processReceipt.ts
+++ src/lib/processReceipt.ts
@@ -1,5 +1,5 @@
 const processReceipt = async (imgURL: string, userId: string) => {
-  return await fetch(`http://localhost:3000/api/test`, {
+  return await fetch(`https://handy-dollar.vercel.app/api/test`, {
     method: "POST",
     body: JSON.stringify({ imgURL, userId }),
     cache: "no-store",
