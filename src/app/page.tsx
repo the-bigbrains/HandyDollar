2fc7fed962175940243d023eb24ff297ba8a8f04
Sim Ho <simhozebs@gmail.com>
2023-09-19 11:45:52 -0400

    temp auth fix and middleware removal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
-import AuthForm from "./auth-form";
+"use client";
+import { supabaseClient } from "@/lib/supabaseClient";
 
 export default function Home() {
+  async function signInWithGitHub() {
+    const getURL = () => {
+      let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/";
+      // Make sure to include `https://` when not localhost.
+      url = url.includes("http") ? url : `https://${url}`;
+      // Make sure to include a trailing `/`.
+      url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+      return url + "dashboard";
+    };
+
+    console.log("geturl", getURL());
+
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: getURL(),
+      },
+    });
+
+    console.log("data", data);
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full text-center">
       <div className="col-6">
@@ -12,7 +38,14 @@ export default function Home() {
         </p>
       </div>
       <div className="col-6 auth-widget">
-        <AuthForm />
+        <button
+          className="bg-white"
+          onClick={async () => {
+            await signInWithGitHub();
+          }}
+        >
+          sign in
+        </button>
       </div>
     </div>
   );
